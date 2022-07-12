145204d989b5747718f1ea8e172dbd01837ca877
Saied Gaffer <saied2421998@gmail.com>
2022-07-12 20:03:51 +0200

    fix hidden loading

--- src/components/Greeting/Greeting.js
+++ src/components/Greeting/Greeting.js
@@ -2,8 +2,8 @@ import { useSelector, shallowEqual } from 'react-redux';
 
 const Greeting = () => {
   const { greeting } = useSelector((state) => state.greetingReducer.greeting, shallowEqual);
-  const { pending } = useSelector((state) => state.greetingReducer.pending, shallowEqual);
-  const { error } = useSelector((state) => state.greetingReducer.error, shallowEqual);
+  const pending = useSelector((state) => state.greetingReducer.pending, shallowEqual);
+  const error = useSelector((state) => state.greetingReducer.error, shallowEqual);
   return (
     <>
       {greeting ? <p>{greeting}</p> : ''}
